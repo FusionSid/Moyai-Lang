d0a03044ac3650e9a2951b71dfa350447bfd6d32
FusionSid <siddheshadsv@icloud.com>
2022-09-13 22:21:41 +1200

    how to use

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-Wanted to make my own language so im making this
+# :moyai: Lang
 
-Currently it supports math expressions and printing them
-
-It has 4 datatypes String, int, float and char.
-
+Wanted to make my own language/compiler so i'm making this  
+Currently it supports math expressions and printing them  
+It has 4 datatypes String, int, float and char.  
 As of now its an interpreted language but once i figure it out it will be a compiled lang
+
+### Usage:
+
+```bash
+make
+./main.out <filename>
+```
--- main.c
+++ main.c
@@ -1,10 +1,12 @@
-// To do later lol
+/*
+Run interpreter (or compiler when I make it):
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// some lex stuff
+// external things ill need
 extern int yyparse();
 extern int yylineno;
 extern char *yytext;
@@ -13,7 +15,7 @@ extern FILE *yyin;
 // Main method be like :moyai:
 int main(int argc, char *argv[])
 {
-    // Check if they provided filename
+    // Check if they provided filename, if they didnt FUCK em
     if (argv[1] == NULL)
     {
         printf("you must provide filename\n");
@@ -29,9 +31,11 @@ int main(int argc, char *argv[])
         printf("File %s not found!\n(Skill issue get good at typing)\n", argv[1]);
         return 1;
     }
-    
+
+    // Parse
     yyparse();
 
+    // CLOSE FILE
     fclose(yyin);
     return 0;
 }
